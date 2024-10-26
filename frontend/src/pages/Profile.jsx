96977a40f4ec6512fe374a4325873c0c63369930
Ankita Sontakke <sontakkeankita70@gmail.com>
2024-10-26 19:52:40 +0530

    Sidebar section UI Built

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React,{useEffect , useState} from 'react'
+import Sidebar from "../components/Profile/Sidebar";
+import Loader from "../components/Loader/Loader";
+import {Outlet} from "react-router-dom";
+import {useSelector} from "react-redux";
+import axios from "axios";
 
 const Profile = () => {
+  // const isLoggedIn = useSelector();
+  const [Profile , setProfile] = useState();
+  const headers = {
+    id:localStorage.getItem("id"),
+    authorization:`Bearer ${localStorage.getItem("token")}`,
+  };
+  useEffect(() => {
+    const fetch = async() => {
+      const response = await axios.get(
+        "http://localhost:3000/api/v1/get-user-information",
+        {headers}
+      );
+      setProfile(response.data);
+    };
+    fetch();
+  },[]);
+
   return (
-    <div>Profile</div>
+    <div className = "bg-zinc-900 px-2 md:px-12 flex flex-col md:flex-row h-screen py-8 gap-4 text-white">
+      {!Profile && (
+        <div className = "w-full h-[100%] flex items-center justify-center">
+        <Loader />
+        </div>
+      )}
+      {Profile && <>
+      <div className = "w-full md:w-1/6">
+        <Sidebar data ={Profile}/>
+      </div>
+      <div className = "w-full md:w-5/6">
+        <Outlet/>
+      </div>
+      </>}
+      </div>
   )
 }
 

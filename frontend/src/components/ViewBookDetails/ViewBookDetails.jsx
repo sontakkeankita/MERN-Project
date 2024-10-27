5ff9961e1482745331daa534291b43a06d451474
Ankita Sontakke <sontakkeankita70@gmail.com>
2024-10-27 22:31:41 +0530

    user side favourite and cart done 5.42.30

--- frontend/src/components/ViewBookDetails/ViewBookDetails.jsx
+++ frontend/src/components/ViewBookDetails/ViewBookDetails.jsx
@@ -4,10 +4,15 @@ import { useParams } from "react-router-dom";
 import Loader from "../Loader/Loader"; // Make sure to import Loader if it's being used
 import { GrLanguage } from "react-icons/gr"; // Import the GrLanguage icon if it's used
 import { FaHeart, FaShoppingCart } from "react-icons/fa";
+import {useSelector} from "react-redux";
 
 const ViewBookDetails = () => {
   const { id } = useParams();
-  const [data, setData] = useState(null); // Use null as the initial state
+  const [data, setData] = useState(null);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn); 
+  const role = useSelector((state) => state.auth.role);
+  console.log(isLoggedIn);
+  console.log(role);
 
   useEffect(() => {
     const fetch = async () => {
@@ -23,22 +28,24 @@ const ViewBookDetails = () => {
   return (
     <>
       {data ? (
-        <div className="px-4 md:px-12 py-8 bg-zinc-900 flex flex-col md:flex-row gap-8 items-start">
+        <div className="px-4 md:px-12 py-8 bg-zinc-900 flex flex-col lg:flex-row gap-8 items-start">
           <div className="w-full lg:w-3/6">
-            <div className="flex justify-around bg-zinc-800 p-12 rounded">
+            <div className="flex flex-col lg:flex-row justify-around bg-zinc-800 p-12 rounded">
               <img
                 src={data.url}
-                alt={data.title}
-                className="h-[50vh] lg:h-[70vh] rounded object-cover"
+                alt="/"
+                className="h-[50vh] md:h-[60vh] lg:h-[70vh] rounded"
               />
-              <div className="flex md:flex-col">
-                <button className="bg-white rounded-full text-3xl p-3 text-red-500">
-                  <FaHeart />
+              {isLoggedIn === true && role === "user" && (
+                <div className="flex flex-row lg:flex-col items-center justify-between lg:justify-start mt-8 lg:mt-0">
+                <button className="bg-white rounded lg:rounded-full text-3xl p-3 text-red-500 flex items-center justify-center">
+                  <FaHeart /> <span className = "ms-4 block lg:hidden">Add to cart</span>
                 </button>
-                <button className="bg-white rounded-full text-3xl p-3 mt-8 text-blue-500">
-                  <FaShoppingCart />
+                <button className="text-white rounded lg:rounded-full text-3xl p-3 mt-0 lg:mt-8 bg-blue-500 flex items-center justify-center">
+                  <FaShoppingCart /> <span className = "ms-4 block lg:hidden">Add to cart</span>
                 </button>
               </div>
+              )}
             </div>
           </div>
           <div className="p-4 w-full lg:w-3/6">

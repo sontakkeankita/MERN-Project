5bb95d7e31d336e2923199a43805ab9e8a98ff05
Ankita Sontakke <sontakkeankita70@gmail.com>
2024-11-06 10:42:20 +0530

    OrderHistory chart heading done

--- frontend/src/components/Profile/UserOrderHistory.jsx
+++ frontend/src/components/Profile/UserOrderHistory.jsx
@@ -37,7 +37,36 @@ const UserOrderHistory = () => {
         </div>
       </div> 
     )}
+  {OrderHistory && OrderHistory.length > 0 && (
+    <div className = "h-[100%] p-0 md:p-4 text-zinc-100">
+      <h1 className = "text-3xl md:text-5xl font-semibold text-zinc-500 mb-8"> Your Order History</h1>
+      <div className = "mt-4 bg-zinc-800 w-full rounded py-2 px-4 flex gap-2">
+        <div className = "w-[3%]">
+          <h1 className = "text-center"> Sr.</h1>
+        </div>
+        <div className = "w-[22%]">
+          <h1 className = ""> Books</h1>
+        </div>
+        <div className = "w-[45%]">
+          <h1 className = ""> Description</h1>
+        </div>
+        <div className = "w-[9%]">
+          <h1 className = ""> Price</h1>
+        </div>
+        <div className = "w-[16%]">
+          <h1 className = ""> Status</h1>
+        </div>
+        <div className = "w-none md:w-[5%] hidden md:block">
+          <h1 className = ""> Mode</h1>
+        </div>
 
+      </div>
+
+       
+        
+       
+    </div>
+  )}
     </>
   )
 };

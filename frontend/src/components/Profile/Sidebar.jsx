b1ee759186f7b1b55cd55bbe19c9c70f828dddad
Ankita Sontakke <sontakkeankita70@gmail.com>
2024-11-06 12:12:49 +0530

    user role and log out functionality done 7.5

--- frontend/src/components/Profile/Sidebar.jsx
+++ frontend/src/components/Profile/Sidebar.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate} from 'react-router-dom';
 import { FaSignOutAlt } from 'react-icons/fa'; // Updated to the correct icon
+import {useDispatch , useSelector} from "react-redux";
+import{authActions} from "../../store/auth";
+
 
 const Sidebar = ({ data }) => {
+  const dispatch = useDispatch();
+  const history = useNavigate();
   return (
     <div className="bg-zinc-800 p-4 rounded flex flex-col items-center justify-between h-auto lg:h-[100%]">
       <div className="flex items-center flex-col justify-center">
@@ -34,9 +39,20 @@ const Sidebar = ({ data }) => {
           Settings
         </Link>
       </div>
-      <button className="bg-zinc-900 w-3/6 lg:w-full mt-4 lg:mt-0 text-white font-semibold flex items-center justify-center py-2 rounded hover:bg-white hover:text-zinc-900 transition-all duration-300">
-        Log Out <FaSignOutAlt className="ms-4" /> {/* Updated icon usage */}
-      </button>
+      <button
+  className="bg-zinc-900 w-3/6 lg:w-full mt-4 lg:mt-0 text-white font-semibold flex items-center justify-center py-2 rounded hover:bg-white hover:text-zinc-900 transition-all duration-300"
+  onClick={() => {
+    dispatch(authActions.logout());
+    dispatch(authActions.changeRole("user"));
+    localStorage.removeItem("id");
+    localStorage.removeItem("token");
+    localStorage.removeItem("role");
+    history("/");
+  }}
+>
+  Log Out <FaSignOutAlt className="ms-4" />
+</button>
+
     </div>
   );
 };
